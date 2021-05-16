2b3ebccf33dba32508d8d38d6873dac77904423c
Vijith <vijith.messi@gmail.com>
2021-05-16 10:22:50 +0000

    2.List Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,11 +3,23 @@ pragma solidity ^0.5.0;
 /**
   * The TodoList contract does this and that...
   */
- contract TodoList {
-   
-   uint public taskCount=0; 
+contract TodoList {
 
-   constructor() public {
-     
-   }
- }
+	uint public taskCount=0; 
+
+	struct Task{
+		uint id;
+		string content;
+		bool completed;
+	}
+	mapping (uint => Task) public tasks;
+
+	constructor() public {
+	 	createTask("Drink Water");
+	}
+	function createTask (string memory _content) public {
+		taskCount++;
+		tasks[taskCount]=Task(taskCount,_content,false);
+	}
+
+}
