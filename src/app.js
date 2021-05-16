f4a915c73e37e7fdcc4b15827d5ba44b366cc8d0
Vijith <vijith.messi@gmail.com>
2021-05-16 11:14:43 +0000

    3.Add and toggle tasks

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const Web3 = require('web3')
-
-App={
+App = {
   loading: false,
   contracts: {},
 
-
-	load:async()=>{
-		await App.loadWeb3()
+  load: async () => {
+    await App.loadWeb3()
     await App.loadAccount()
     await App.loadContract()
     await App.render()
-	},
-	loadWeb3: async () => {
+  },
+
+  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider
       web3 = new Web3(web3.currentProvider)
@@ -33,6 +33,7 @@ App={
     // Legacy dapp browsers...
     else if (window.web3) {
       App.web3Provider = web3.currentProvider
+      web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
       window.web3 = new Web3(web3.currentProvider)
       // Acccounts always exposed
       web3.eth.sendTransaction({/* ... */})
@@ -42,17 +43,24 @@ App={
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   },
-  loadAccount:async () =>{
-    App.account=web3.eth.accounts[0]
-    console.log(App.account)
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+    web3.eth.defaultAccount=web3.eth.accounts[0]
+    App.account = web3.eth.defaultAccount
   },
-  loadContract: async () =>{
-    const todoList=await $.getJSON('TodoList.json')
-    App.contracts.TodoList=TruffleContract(todoList)
+
+  loadContract: async () => {
+    // Create a JavaScript version of the smart contract
+    const todoList = await $.getJSON('TodoList.json')
+    App.contracts.TodoList = TruffleContract(todoList)
     App.contracts.TodoList.setProvider(App.web3Provider)
-    App.todoList=await App.contracts.TodoList.deployed()
+
+    // Hydrate the smart contract with values from the blockchain
+    App.todoList = await App.contracts.TodoList.deployed()
   },
-render: async () => {
+
+  render: async () => {
     // Prevent double render
     if (App.loading) {
       return
@@ -132,8 +140,8 @@ render: async () => {
   }
 }
 
-$(()=>{
-	$(window).load(()=>{
-		App.load()
-	})
+$(() => {
+  $(window).load(() => {
+    App.load()
+  })
 })
