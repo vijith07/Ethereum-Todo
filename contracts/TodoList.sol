90be49ff34188c74743240949650c1ecb114bbcc
Vijith <vijith.messi@gmail.com>
2021-05-16 08:24:53 +0000

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+/**
+  * The TodoList contract does this and that...
+  */
+ contract TodoList {
+   
+   uint public taskCount=0; 
+
+   constructor() public {
+     
+   }
+ }
